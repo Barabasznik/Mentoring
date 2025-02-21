eefb115564560593129de2ed43e08e64aae329a3
Barabasznik <adrianbarabasz9@gmail.com>
2025-02-21 12:02:04 +0100

    Dodanie Book

---
+++ Mentoring.Server.DataAcces/DataAccesRegistry.cs
@@ -0,0 +1,13 @@
+﻿using Mentoring.Server.DataAcces.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mentoring.Server.DataAcces
+{
+    public static class DataAccesRegistry
+    {
+        public static void RegisterDataAcces(this IServiceCollection services)
+        {
+            services.AddScoped<IBookRepository, BookRepository>();
+        }
+    }
+}
