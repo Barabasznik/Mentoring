23f54a02721691a67d0deb178d07c967ae23e300
Barabasznik <adrianbarabasz9@gmail.com>
2025-02-24 19:57:42 +0100

    Domowe nr

---
+++ Mentoring.Server.DataAcces/BookRepository.cs
@@ -0,0 +1,15 @@
+﻿using Mentoring.Server.DataAcces.Models;
+using Mentoring.Server.DataAcces.Repositories;
+
+
+namespace Mentoring.Server.DataAcces
+{
+    internal class BookRepository : IBookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>() { new Book() { Title = "wahtever" } };
+
+        }
+    }
+}
