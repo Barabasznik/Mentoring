ea818821ee77579ca3296918ec2fd343bcc833a3
Barabasznik <adrianbarabasz9@gmail.com>
2025-03-03 17:20:23 +0100

    git

--- Mentoring.Server.DataAcces/Models/Book.cs
+++ Mentoring.Server.DataAcces/Models/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mentoring.Server.DataAcces.Models
+﻿namespace Mentoring.Server.DataAcces.Models
 {
     public class Book
     {
