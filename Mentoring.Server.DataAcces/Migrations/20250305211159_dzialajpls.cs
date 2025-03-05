d3cc6dcbfe3a2cc80d6f1d2be4f215849b013b40
Barabasznik <adrianbarabasz9@gmail.com>
2025-03-05 22:26:31 +0100

    działaaaa

---
+++ Mentoring.Server.DataAcces/Migrations/20250305211159_dzialajpls.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mentoring.Server.DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class dzialajpls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
